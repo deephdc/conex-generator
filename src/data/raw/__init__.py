8ca839d5c025881fd7f1f723616363af2dafefce
Marcel Köpke <marcel.koepke@gmail.com>
2019-10-25 18:07:04 +0200

    convert all raw files in one run

--- src/data/raw/__init__.py
+++ src/data/raw/__init__.py
@@ -1,3 +1,3 @@
 from .path import get_path
-
+from .convert_run import convert_run
 from . import json_to_numpy
---
+++ src/data/raw/convert_run.py
@@ -0,0 +1,28 @@
+import numpy as np
+import os
+import glob
+
+import src.utils
+log = src.utils.getLogger(__name__)
+
+from . import get_path
+rawpath = get_path()
+
+from . import json_to_numpy
+
+
+def convert_run(run, expand_depth=True, overwrite=False):
+    runpath = os.path.join(rawpath, run)
+
+    if not os.path.isdir(runpath):
+        msg = f"{runpath} does not exist"
+        log.error(msg)
+        raise IOError(msg)
+
+    json_files = glob.glob(os.path.join(runpath, "*.json"))
+
+    for filepath in json_files:
+        filename = os.path.split(filepath)[-1]
+        dataobject = json_to_numpy.create_dataobject(filename, run, expand_depth)
+        json_to_numpy.store_dataobject(dataobject, overwrite)
+
