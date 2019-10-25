a2ef44220622f4ab09bdddd6582487363e43ca74
Marcel Köpke <marcel.koepke@gmail.com>
2019-10-25 17:37:20 +0200

    json to numpy testing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import src
+import matplotlib
+import matplotlib.pyplot as plt
+import logging
 
-log = src.utils.getLogger(__name__)
+import src
+log = src.utils.getLogger(__name__, logging.INFO)
 
 log.debug("debug message")
 log.info("info message")
 log.warning("warning message")
 log.error("error message")
 log.critical("critical message")
+
+test = src.data.raw.json_to_numpy.create_dataobject("conex_data_2019-10-24-18-30-19.604496.json", "run02", expand_depth=False)
+src.data.raw.json_to_numpy.store_dataobject(test, overwrite=True)
 
