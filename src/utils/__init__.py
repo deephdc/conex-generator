f5f47b840dae74a91a50b7e897e3834e5d6f50fa
Marcel Köpke <marcel.koepke@gmail.com>
2019-11-03 15:18:32 +0100

    better logging level setter

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
-from .log import getLogger
+from .log import getLogger, setLevel
 from .path import get_utils_path as get_path
 from .path import get_root_path
 from .timestamp import timestamp
--- src/utils/log.py
+++ src/utils/log.py
@@ -12,10 +12,36 @@ logging.basicConfig(
 
 def getLogger(name = None, level = None):
     if level is not None:
-        logging.getLogger().setLevel(level)
+        setLevel(level)
 
     if name is None:
         return logging.getLogger()
 
     return logging.getLogger(name)
+
+
+def setLevel(level : str):
+    level = level.lower()
+    logger = logging.getLogger()
+
+    if level == "critical" or level == "c":
+        logger.setLevel(logging.CRITICAL)
+        return
+    if level == "error" or level == "e":
+        logger.setLevel(logging.ERROR)
+        return
+    if level == "warning" or level == "w":
+        logger.setLevel(logging.WARNING)
+        return
+    if level == "info" or level == "i":
+        logger.setLevel(logging.INFO)
+        return
+    if level == "debug" or level == "d":
+        logger.setLevel(logging.DEBUG)
+        return
+    if level == "notset" or level == "n":
+        logger.setLevel(logging.NOTSET)
+        return
+
+    raise RuntimeError("unsupported logging level")
 
