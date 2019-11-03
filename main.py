e50cd09747349010ad2075d18ac3a5df64630272
Marcel Köpke <marcel.koepke@gmail.com>
2019-11-03 15:21:54 +0100

    new main

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ import matplotlib.pyplot as plt
 import logging
 
 import src
-log = src.utils.getLogger(__name__, logging.INFO)
+log = src.utils.getLogger(__name__, "info")
 
-run = "run02"
-#src.data.interim.clear_run(run)
-#src.data.processed.clear_run(run)
-#src.data.raw.convert_run(run, expand_depth=False)
-#src.data.interim.merge_run(run, expand_depth=False)
-
-dataset, metadata = src.data.processed.load_data(run)
+run = "run01"
+src.data.clear_run(run)
+src.data.raw.convert_run(run, expand_depth=True)
+src.data.interim.merge_run(run, expand_depth=True)
 
