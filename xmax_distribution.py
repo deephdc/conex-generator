296d8c9fec79f027ee9e5b1ceec56cfa731a4d15
Marcel Köpke <marcel.koepke@gmail.com>
2019-11-06 23:43:50 +0100

    working xmax distribution script

--- xmax_distribution.py
+++ xmax_distribution.py
@@ -9,13 +9,12 @@ model_base = "models/gan/run01/02"
 root_path = src.utils.get_root_path()
 model_path = os.path.join(root_path, model_base)
 plot_path = os.path.join(model_path, "plots", src.utils.timestamp())
-#os.mkdir(plot_path)
 
 gdata = np.load(os.path.join(model_path, "gdata.npy"))
 rdata = np.load(os.path.join(model_path, "rdata.npy"))
 label = np.load(os.path.join(model_path, "label.npy"))
 
-# filter data
+# prepare data
 condition1 = label[:,0] >= 2
 print("condition1: ", np.sum(condition1))
 condition2 = label[:,1] >= 1e9
@@ -43,7 +42,39 @@ rdata = rdata[:,0:dcut,:]
 gdata = np.abs(gdata) + 0.01
 rdata = np.abs(rdata) + 0.01
 
+numdata = rdata.shape[0]
+channel = [0,1,2,3,4,5,6]
 depth = np.arange(rdata.shape[1])*10.0 + 10.0
 
-params = src.analysis.xmax.gaisser_hillas_fit(depth, rdata[0,:,0])
+# calculate distributions
+gparam = np.zeros((numdata, len(channel), 4))
+rparam = np.zeros((numdata, len(channel), 4))
+for ii in range(numdata):
+    for jj in range(len(channel)):
+        gparam[ii,jj,:] = src.analysis.xmax.gaisser_hillas_fit(depth, gdata[ii,:,jj])
+        rparam[ii,jj,:] = src.analysis.xmax.gaisser_hillas_fit(depth, rdata[ii,:,jj])
+
+gxmax = gparam[:,:,1]
+rxmax = rparam[:,:,1]
+
+print(np.min(gxmax))
+print(np.min(rxmax))
+
+# plots
+os.mkdir(plot_path)
+
+bins = np.linspace(600,1200,21)
+
+for jj in range(len(channel)):
+    filename = "gxmax_" + str(jj) + ".png"
+    fig = plt.figure()
+    plt.hist(gxmax[:,jj], bins=bins)
+    plt.savefig(os.path.join(plot_path, filename))
+    plt.close(fig)
+
+    filename = "rxmax_" + str(jj) + ".png"
+    fig = plt.figure()
+    plt.hist(rxmax[:,jj], bins=bins)
+    plt.savefig(os.path.join(plot_path, filename))
+    plt.close(fig)
 
