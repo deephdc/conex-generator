849ade2e01d5533e353d5df0df42af214116a647
Marcel Köpke <marcel.koepke@gmail.com>
2019-11-07 10:55:21 +0100

    moved and improved gaiser hillas fit

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,1 +1,4 @@
-from . import xmax
+from .gaisser_hillas_fit import gaisser_hillas
+from .gaisser_hillas_fit import gaisser_hillas_log
+from .gaisser_hillas_fit import gaisser_hillas_fit
+
--- src/analysis/xmax/__init__.py
+++ src/analysis/xmax/__init__.py
@@ -1,2 +1,4 @@
+from .gaisser_hillas_fit import gaisser_hillas
+from .gaisser_hillas_fit import gaisser_hillas_log
 from .gaisser_hillas_fit import gaisser_hillas_fit
 
