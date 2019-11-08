195b85da3a86adefb5419fa397afe1b755ed5638
Marcel Köpke <marcel.koepke@gmail.com>
2019-11-08 21:10:43 +0100

    fit stablelized

--- src/analysis/gaisser_hillas_fit.py
+++ src/analysis/gaisser_hillas_fit.py
@@ -3,16 +3,33 @@ import scipy as sp
 import scipy.optimize as opt
 
 
-bound_x0 = 200.0
-bound_lam = 1000.0
+# bounds
+bound_x0_upper = 200.0
+bound_x0_lower = -200.0
+scale_x0 = 1000.0
+zero_x0 = -100.0
+
+bound_lam_upper = 1000.0
+bound_lam_lower = 1.0
+scale_lam = 1000.0
+zero_lam = 50.0
+
+# preprocessing
+range_x0 = bound_x0_upper - bound_x0_lower
+shift_x0 = range_x0 - bound_x0_upper
+inverse_x0 = np.arctanh((zero_x0 + shift_x0)*2.0/range_x0 - 1.0)*scale_x0
+
+range_lam = bound_lam_upper - bound_lam_lower
+shift_lam = range_lam - bound_lam_upper
+inverse_lam = np.arctanh((zero_lam + shift_lam)*2.0/range_lam - 1.0)*scale_lam
 
 
 def transform_param(p1, p2, p3, p4):
     nmax = np.abs(p1)
     xmax = np.abs(p2)
-    x0 =   bound_x0 * (np.tanh((p3 -    0.0)/1000.0)-0.0)/1.0 - 0.0
-    lam = bound_lam * (np.tanh((p4 - 1000.0)/1000.0)+1.0)/2.0 + 1.0
-    
+    x0 =  range_x0  * (np.tanh((p3 + inverse_x0) /scale_x0) +1.0)/2.0 - shift_x0
+    lam = range_lam * (np.tanh((p4 + inverse_lam)/scale_lam)+1.0)/2.0 - shift_lam
+
     return (nmax, xmax, x0, lam)
 
 
@@ -130,7 +147,7 @@ def fit_nmax_xmax_x0_lam(dep, dat, nmax, xmax, x0, lam):
 
 
 def gaisser_hillas_fit(depth, data):
-    ind = np.where(depth > bound_x0)[0]
+    ind = np.where(depth >= bound_x0_upper + 50.0)[0]
     dep = depth[ind]
     dat = data[ind]
 
