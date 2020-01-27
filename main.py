62415e19f99b7e1b2f04b570c71c1635d9b11110
Marcel Köpke <marcel.koepke@gmail.com>
2020-01-27 13:51:25 +0100

    conversion with align

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import src
 log = src.utils.getLogger(__name__, "info")
 
 run = "run01"
-src.data.clear_run(run)
+src.data.clear_run(run + "*")
 src.data.raw.convert_run(run, expand_depth=True)
+src.data.interim.align_run(run, expand_depth=True)
 src.data.interim.merge_run(run, expand_depth=True)
 
