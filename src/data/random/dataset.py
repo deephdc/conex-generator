97be5a078f305c54710cdadf057fdf973d19d854
Marcel Köpke <marcel.koepke@gmail.com>
2020-03-17 18:16:34 +0100

    default dtype is now np.float32 for lazy loading of tf

--- src/data/random/dataset.py
+++ src/data/random/dataset.py
@@ -2,12 +2,16 @@ import lazy_import
 
 tf = lazy_import.lazy_module("tensorflow")
 #import tensorflow as tf
+import numpy as np
+
+import src.utils
+log = src.utils.getLogger(__name__)
 
 
-def uniform_dataset(shape=(1,), minval=-1.0, maxval=1.0, dtype=tf.float32,
+def uniform_dataset(shape=(1,), minval=-1.0, maxval=1.0, dtype=np.float32,
                     buffsize=131072):
     ds = tf.data.Dataset.from_generator(
-            uniform_generator[dtype], output_types=dtype,
+            uniform_generator(dtype), output_types=dtype,
             output_shapes=(buffsize, *shape),
             args=[(buffsize, *shape), minval, maxval])
     ds = ds.unbatch().repeat()
@@ -18,15 +22,19 @@ def uniform_generator_float32(shape, minval, maxval, runs=1024):
         yield tf.random.uniform(shape=shape, minval=minval, maxval=maxval,
                                 dtype=tf.float32)
 
-uniform_generator = {
-        tf.float32: uniform_generator_float32,
-}
+def uniform_generator(dtype):
+    if dtype == tf.float32:
+        return uniform_generator_float32
+    
+    msg = "uniform random generator does not support this dtype"
+    log.critical(msg)
+    raise NotImplementedError(msg)
 
 
-def normal_dataset(shape=(1,), mean=0.0, stddev=1.0, dtype=tf.float32,
+def normal_dataset(shape=(1,), mean=0.0, stddev=1.0, dtype=np.float32,
                    buffsize=131072):
     ds = tf.data.Dataset.from_generator(
-            normal_generator[dtype], output_types=dtype,
+            normal_generator(dtype), output_types=dtype,
             output_shapes=(buffsize, *shape),
             args=[(buffsize, *shape), mean, stddev])
     ds = ds.unbatch().repeat()
@@ -37,15 +45,19 @@ def normal_generator_float32(shape, mean, stddev, runs=1024):
         yield tf.random.normal(shape=shape, mean=mean, stddev=stddev,
                                dtype=tf.float32)
 
-normal_generator = {
-        tf.float32: normal_generator_float32,
-}
+def normal_generator(dtype):
+    if dtype == tf.float32:
+        return normal_generator_float32
+    
+    msg = "normal random generator does not support this dtype"
+    log.critical(msg)
+    raise NotImplementedError(msg)
 
 
-def truncated_normal_dataset(shape=(1,), mean=0.0, stddev=1.0, dtype=tf.float32,
+def truncated_normal_dataset(shape=(1,), mean=0.0, stddev=1.0, dtype=np.float32,
                              buffsize=131072):
     ds = tf.data.Dataset.from_generator(
-            truncated_normal_generator[dtype], output_types=dtype,
+            truncated_normal_generator(dtype), output_types=dtype,
             output_shapes=(buffsize, *shape),
             args=[(buffsize, *shape), mean, stddev])
     ds = ds.unbatch().repeat()
@@ -55,7 +67,12 @@ def truncated_normal_generator_float32(shape, mean, stddev, runs=1024):
     for ii in range(runs):
         yield tf.random.truncated_normal(shape=shape, mean=mean, stddev=stddev,
                                          dtype=tf.float32)
-truncated_normal_generator = {
-        tf.float32: truncated_normal_generator_float32,
-}
+
+def truncated_normal_generator(dtype):
+    if dtype == tf.float32:
+        return truncated_normal_generator_float32
+    
+    msg = "truncated normal random generator does not support this dtype"
+    log.critical(msg)
+    raise NotImplementedError(msg)
 
