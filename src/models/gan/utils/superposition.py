7d8656129ca1427650e6c863ff15ea2017bee3a2
Marcel Köpke <marcel.koepke@gmail.com>
2020-03-25 22:30:37 +0100

    outsourced superposition and refactored for any shape

---
+++ src/models/gan/utils/superposition.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import numpy as np
+
+
+class UniformSuperposition(tf.keras.layers.Layer):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert input_shape[0] == input_shape[1]
+        self._epsilon_shape = np.ones_like(input_shape[0])
+        self._epsilon_shape[0] = -1
+        super().build(input_shape)
+
+    @tf.function
+    def call(self, inputs, training=False):
+        data0 = inputs[0]
+        data1 = inputs[1]
+
+        epsilon_shape = tf.where(self._epsilon_shape == -1, tf.shape(data0)[0], self._epsilon_shape)
+        epsilon = tf.random.uniform(shape=epsilon_shape , dtype=tf.float32)
+
+        tensor = epsilon * data0 + (1 - epsilon) * data1
+        return tensor
+
