4327de7a5f44b703b185ea4f6cc60b3aab4ba5d3
Marcel Köpke <marcel.koepke@gmail.com>
2020-03-25 22:33:19 +0100

    generator with depthlen fix and different noise handling

--- src/models/gan/generator.py
+++ src/models/gan/generator.py
@@ -1,40 +1,47 @@
 import tensorflow as tf
 
-from .utils.datamerger import DataSplitter
-from .utils.datanormalizer import DataDenormalizer
-from .utils.labelmerger import LabelMerger
+from .utils import DataSplitter
+from .utils import DataDenormalizer
+from .utils import LabelMerger
+
+
+def get_maxdepthlen():
+    return 2*2*2*2*2*3*3 # 288
 
 
 class Generator(tf.keras.Model):
 
-    def __init__(self, pd_maxdata, ed_maxdata, pd_feature_list=None,
+    def __init__(self, depthlen, pd_maxdata, ed_maxdata, pd_feature_list=None,
                  ed_feature_list=None, numparticle=6, **kwargs):
         super().__init__(**kwargs)
 
+        self.depthlen = depthlen
+        self.maxdepthlen = get_maxdepthlen()
         self.pd_maxdata = pd_maxdata
         self.ed_maxdata = ed_maxdata
         self._numparticle = numparticle
-
 
         self.datasplitter = DataSplitter(pd_feature_list, ed_feature_list)
         self.denormalizer = DataDenormalizer(self.pd_maxdata, self.ed_maxdata)
         self.gen_features = self.datasplitter.gen_features
 
-        self.dense_generator = DenseGenerator(self.gen_features,
+        self.dense_generator = DenseGenerator(self.maxdepthlen,
+                                              self.gen_features,
                                               self._numparticle)
 
     @tf.function
     def call(self, inputs, training=False):
         label = inputs[0]
-        noise1 = inputs[1]
+        noise = inputs[1]
 
         # run different generators
-        output1 = self.dense_generator((label,noise1,))
+        output1 = self.dense_generator((label,noise,))
 
         # merge outputs
         tensor = output1
 
         # format data
+        tensor = tensor[:,0:self.depthlen,:]
         tensor = self.datasplitter(tensor)
         tensor = self.denormalizer(tensor)
 
@@ -42,9 +49,11 @@ class Generator(tf.keras.Model):
 
 
 class DenseGenerator(tf.keras.Model):
-    def __init__(self, gen_features=8, numparticle=6, **kwargs):
+
+    def __init__(self, depthlen=288, gen_features=8, numparticle=6, **kwargs):
         super().__init__(**kwargs)
 
+        self.depthlen = depthlen
         self.gen_features = gen_features
 
         self.labelmerger = LabelMerger(numparticle=numparticle)
@@ -53,15 +62,15 @@ class DenseGenerator(tf.keras.Model):
 
         self.layer1 = tf.keras.layers.Dense(512)
         self.layer2 = tf.keras.layers.Dense(1024)
-        self.layer3 = tf.keras.layers.Dense(self.gen_features * 300)
+        self.layer3 = tf.keras.layers.Dense(self.depthlen * self.gen_features)
 
     @tf.function
     def call(self, inputs, training=False):
         label = inputs[0]
-        noise1 = inputs[1]
+        noise = inputs[1][0]
 
         labeltensor = self.labelmerger(label)
-        tensor = tf.concat([labeltensor, noise1], -1)
+        tensor = tf.concat([labeltensor, noise], -1)
 
         tensor = self.layer1(tensor)
         tensor = self.activation(tensor)
@@ -71,7 +80,7 @@ class DenseGenerator(tf.keras.Model):
 
         tensor = self.layer3(tensor)
         tensor = tf.keras.activations.sigmoid(tensor) * 1.5
-        tensor = tf.reshape(tensor, shape=[-1, 300, self.gen_features])
+        tensor = tf.reshape(tensor, shape=[-1, self.depthlen, self.gen_features])
 
         return tensor
 
