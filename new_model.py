778a72b38ef6693abb0441e7d560dba2c23fcab4
Marcel Köpke <marcel.koepke@gmail.com>
2020-04-08 16:37:06 +0200

    faster maxdata estimate

--- new_model.py
+++ new_model.py
@@ -65,16 +65,24 @@ ds = tf.data.Dataset.zip((
 ds : tf.data.Dataset = ds.shuffle(100000).batch(1024).prefetch(5)
 
 # get data info: maximum estimate, shape
-pd_maxdata = tf.zeros(8, dtype=tf.float32)
-ed_maxdata = tf.zeros(9, dtype=tf.float32)
-for batch in ds.take(100):
-    # pd
-    batchmax = tf.math.reduce_max(tf.abs(batch[1][0]), axis=(0,1))
-    pd_maxdata = tf.where(batchmax > pd_maxdata, batchmax, pd_maxdata)
+@tf.function
+def get_maxdata(dataset):
+    pd_maxdata = tf.zeros(8, dtype=tf.float32)
+    ed_maxdata = tf.zeros(9, dtype=tf.float32)
+    for batch in dataset:
+        # pd
+        batchmax = tf.math.reduce_max(tf.abs(batch[1][0]), axis=(0,1))
+        pd_maxdata = tf.where(batchmax > pd_maxdata, batchmax, pd_maxdata)
 
-    # ed
-    batchmax = tf.math.reduce_max(tf.abs(batch[1][1]), axis=(0,1))
-    ed_maxdata = tf.where(batchmax > ed_maxdata, batchmax, ed_maxdata)
+        # ed
+        batchmax = tf.math.reduce_max(tf.abs(batch[1][1]), axis=(0,1))
+        ed_maxdata = tf.where(batchmax > ed_maxdata, batchmax, ed_maxdata)
+
+    return (pd_maxdata, ed_maxdata)
+pd_maxdata, ed_maxdata = get_maxdata(ds)
+
+pd_maxdata = pd_maxdata.numpy()
+ed_maxdata = ed_maxdata.numpy()
 
 for batch in ds.take(1):
     depthlen  = batch[1][0].shape[1]
