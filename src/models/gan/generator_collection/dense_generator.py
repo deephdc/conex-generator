c0fc00591faab5e835a56011ba3208983417cdf6
Marcel Köpke <marcel.koepke@gmail.com>
2020-06-26 16:57:06 +0200

    dense generator can expscale

--- src/models/gan/generator_collection/dense_generator.py
+++ src/models/gan/generator_collection/dense_generator.py
@@ -7,20 +7,27 @@ from src.models.gan import utils
 class DenseGenerator(tf.keras.layers.Layer):
 
     def __init__(self, depthlen=288, gen_features=8, numparticle=6,
+                 overscale=10.0, expscale=False,
                  activation=tf.keras.activations.tanh, **kwargs):
         super().__init__(**kwargs)
 
         self.depthlen = depthlen
         self.gen_features = gen_features
+        self.overscale = 10.0
+        self.expscale = expscale
 
         self.labelmerger = utils.LabelMerger(numparticle=numparticle)
 
         self.activation = activation
+        if self.expscale:
+            last_activation = tf.keras.activations.elu
+        else:
+            last_activation = tf.keras.activations.sigmoid
 
         self.layer1 = layers.Dense(512, activation=self.activation)
         self.layer2 = layers.Dense(1024, activation=self.activation)
         self.layer3 = layers.Dense(self.depthlen * self.gen_features,
-                                   activation=tf.keras.activations.sigmoid)
+                                   activation=last_activation)
 
     @tf.function
     def call(self, inputs, training=False):
@@ -36,5 +43,10 @@ class DenseGenerator(tf.keras.layers.Layer):
 
         tensor = tf.reshape(tensor, shape=[-1, self.depthlen, self.gen_features])
 
-        return tensor * 10.0
+        if self.expscale:
+            tensor = tf.math.exp(-1.0*(tensor + 1.0 - tf.math.log(self.overscale)))
+        else:
+            tensor = tensor * self.overscale
+
+        return tensor
 
