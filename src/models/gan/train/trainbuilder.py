887fa498c063467e101a7b19d360e84e2d1c61e1
Marcel Köpke <marcel.koepke@gmail.com>
2020-07-02 16:34:48 +0200

    training strategy: single

--- src/models/gan/train/trainbuilder.py
+++ src/models/gan/train/trainbuilder.py
@@ -14,6 +14,7 @@ class TrainBuilder():
         self.model = modelbuilder
 
         self.built = False
+        self.execute_index = 0
 
         # constructions
         self.optimizer_generator : tf.keras.optimizers.Optimizer = None
@@ -63,7 +64,7 @@ class TrainBuilder():
 
         # choose strategy
         if strategy == "single":
-            current_strategy = self.strategy_single
+            current_strategy = self._strategy_single
         else:
             raise NotImplementedError(f"unknown training strategy: {strategy}")
 
@@ -93,14 +94,125 @@ class TrainBuilder():
 
         return self
 
-    def strategy_single(self, epochs):
+    def _train_generator(self,
+                        label,
+                        real,
+                        noise,
+                        generator_ensemble,
+                        discriminator_ensemble):
+        generator : tf.keras.Model = self.model.generator
+        discriminator : tf.keras.Model = self.model.discriminator
+        wasserstein_distance : tf.keras.Model = self.model.wasserstein_distance
+
+        optimizer : tf.keras.optimizers.Optimizer = self.optimizer_generator
+
+        generator.ensemble = generator_ensemble
+        discriminator.ensemble = discriminator_ensemble
+
+        with tf.GradientTape(watch_accessed_variables=False) as tape:
+            tape.watch(generator.trainable_weights)
+
+            fake = generator([label, *noise])
+            distance = wasserstein_distance([label, *real, *fake])
+
+            loss = distance
+
+        gradient = tape.gradient(loss, generator.trainable_weights)
+        optimizer.apply_gradients(zip(gradient, generator.trainable_weights))
+
+        return (loss, distance)
+
+    def _train_discriminator(self,
+                            label,
+                            real,
+                            noise,
+                            generator_ensemble,
+                            discriminator_ensemble):
+        generator : tf.keras.Model = self.model.generator
+        discriminator : tf.keras.Model = self.model.discriminator
+        wasserstein_distance : tf.keras.Model = self.model.wasserstein_distance
+        gradient_penalty : tf.keras.Model = self.model.gradient_penalty
+
+        optimizer : tf.keras.optimizers.Optimizer = self.optimizer_discriminator
+
+        generator.ensemble = generator_ensemble
+        discriminator.ensemble = discriminator_ensemble
+
+        with tf.GradientTape(watch_accessed_variables=False) as tape:
+            tape.watch(discriminator.trainable_weights)
+
+            fake = generator([label, *noise])
+            distance = wasserstein_distance([label, *real, *fake])
+            penalty = gradient_penalty([label, *real, *fake])
+
+            loss = - distance + penalty
+
+        gradient = tape.gradient(loss, discriminator.trainable_weights)
+        optimizer.apply_gradients(zip(gradient, discriminator.trainable_weights))
+
+        return (loss, distance, penalty)
+
+    def _make_summary(self,
+                     name,
+                     step,
+                     generator_ensemble,
+                     discriminator_ensemble):
         dataset = self.data.dataset
 
-        generator = self.model.generator
-        discriminator = self.model.discriminator
-        wasserstein_distance = self.model.wasserstein_distance
-        gradient_penalty = self.model.gradient_penalty
+        generator : tf.keras.Model = self.model.generator
+        discriminator : tf.keras.Model = self.model.discriminator
+        wasserstein_distance : tf.keras.Model = self.model.wasserstein_distance
+        gradient_penalty : tf.keras.Model = self.model.gradient_penalty
+
+        generator.ensemble = generator_ensemble
+        discriminator.ensemble = discriminator_ensemble
+
+        distance = 0
+        penalty = 0
+        for label, real, noise in dataset.take(10):
+            fake = generator([label, *noise])
+            distance += wasserstein_distance([label, *real, *fake])
+            penalty += gradient_penalty([label, *real, *fake])
+        distance /= 10
+        penalty /= 10
+
+        tf.summary.scalar(
+                name + " - Wasserstein Distance",
+                distance,
+                step=step)
+
+        tf.summary.scalar(
+                name + " - Gradient Penalty",
+                penalty,
+                step=step)
+
+    def _strategy_single(self, epochs):
+        self.execute_index += 1
+
+        dataset = self.data.dataset
+
+        generator : tf.keras.Model = self.model.generator
+        discriminator : tf.keras.Model = self.model.discriminator
+
+        step = 0
 
         for epoch in range(epochs):
             for ii, (label, real, noise) in dataset.enumerate():
-                pass
+
+                if ii % 6 == 5:
+                    # train generators
+                    for jj in range(generator.num_model):
+                        loss, distance = self._train_generator(
+                                label, real, noise, jj+1, 0)
+
+                else:
+                    # train discriminators
+                    for jj in range(discriminator.num_model):
+                        loss, distance, penalty = self._train_discriminator(
+                                label, real, noise, 0, jj+1)
+
+            # write summary after each epoch
+            self._make_summary(f"{self.execute_index} - single",
+                              epoch, 0, 0)
+
+
