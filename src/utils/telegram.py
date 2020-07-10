6510883436d0d0e9bcbdcb960413eae5d05b0805
Marcel Köpke <marcel.koepke@gmail.com>
2020-07-10 14:31:06 +0200

    telegram config from env vars

--- src/utils/telegram.py
+++ src/utils/telegram.py
@@ -6,20 +6,27 @@ import threading
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 telegram_config_path = os.path.join(script_path, "telegram.json")
+config = dict()
 try:
     with open(telegram_config_path, "r") as fp:
         config = json.load(fp)
 except:
-    pass
+    if "TELEGRAM_BOT_TOKEN" in os.environ and "TELEGRAM_BOT_CHAT_ID" in os.environ:
+        config = {
+                "api-token": os.environ["TELEGRAM_BOT_TOKEN"],
+                "chat-id": os.environ["TELEGRAM_BOT_CHAT_ID"],
+        }
+        with open(telegram_config_path, "w") as fp:
+            json.dump(config, fp, indent=4)
 
 
-def send_to_bot(message):
+def send_to_bot(message, token, chatid):
     try:
         text = "https://api.telegram.org/bot" \
-                + config["api-token"] \
+                + token \
                 + "/sendMessage?chat_id=" \
-                + config["chat-id"] \
-                + "&parse_mode=Markdown&text=" \
+                + chatid \
+                + "&text=" \
                 + message
         response = requests.get(text, timeout=5)
         retval = response.json()
@@ -33,10 +40,22 @@ class TelegramHandler(logging.StreamHandler):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.token = config.get("api-token", None)
+        self.chatid = config.get("chat-id", None)
+
+        if self.token is not None and self.chatid is not None:
+            self.useable = True
+        else:
+            self.useable = False
 
     def emit(self, record):
-        message = self.format(record)
-        thread = threading.Thread(target=send_to_bot, args=(message,))
-        thread.setDaemon(True)
-        thread.start()
+        if self.useable:
+            message = self.format(record)
+
+            thread = threading.Thread(
+                    target=send_to_bot,
+                    args=(message, self.token, self.chatid))
+
+            thread.setDaemon(True)
+            thread.start()
 
