fce29c96dd984fb4475986a10b1b1fd6cf96f744
Marcel Köpke <marcel.koepke@gmail.com>
2020-07-10 11:42:19 +0200

    better version selection with ordering and binary autodetect

--- src/steering/corsika/__init__.py
+++ src/steering/corsika/__init__.py
@@ -1,4 +1,4 @@
-from .path import get_path, get_run_path, get_corsika_binary, set_version, set_corsika_binary
+from .path import get_path, get_run_path, get_binary, set_version, set_binary
 from .config import get_particle_list
 from .steering_file import write_steering_file
 from .steering_file import get_steering_filepath
--- src/steering/corsika/path.py
+++ src/steering/corsika/path.py
@@ -2,41 +2,61 @@ import os
 import glob
 
 from .. import get_install_path
+import src.utils
+log = src.utils.getLogger(__name__)
 
 
+# setup path and runpath
 search_expr = os.path.join(get_install_path(), "corsika-*")
 corsika_paths = glob.glob(search_expr)
 corsika_paths.sort()
 corsika_path = corsika_paths[-1]
 
-corsika_filename = "corsika77000Linux_QGSII_urqmd_thin_conex"
-
-
 def get_path():
     return corsika_path
+
+def get_run_path():
+    return os.path.join(get_path(), "run")
 
 def set_version(version):
     global corsika_path
     newpath = os.path.join(get_install_path(), "corsika-" + version)
     if os.path.isdir(newpath):
         corsika_path = newpath
+        binary = os.path.split(get_binary())[-1].split("_", 1)[-1]
+        print(binary)
+        try:
+            set_binary(binary)
+        except RuntimeError:
+            log.warning(f"cannot find corresponding binary for new corsika version {version}")
     else:
         raise RuntimeError(f"cannot find version corsika-{version}")
 
-def get_run_path():
-    return os.path.join(get_path(), "run")
 
-def get_corsika_binary():
+# setup binary path
+search_expr = os.path.join(get_run_path(), "corsika*Linux_*")
+corsika_filenames = glob.glob(search_expr)
+corsika_filenames.sort()
+corsika_filename = corsika_filenames[-1]
+
+def get_binary():
     runpath = get_run_path()
-    
     return os.path.join(runpath, corsika_filename)
 
-def set_corsika_binary(binary):
+def set_binary(binary):
     global corsika_filename
     binary = os.path.split(binary)[-1]
 
     if os.path.isfile(os.path.join(get_run_path(), binary)):
-        corsika_filename = binary
-    else:
-        raise RuntimeError(f"cannot find corsika binary {binary}")
+        corsika_filename = os.path.join(get_run_path(), binary)
+        return
+
+    search_expr = os.path.join(get_run_path(), "corsika*Linux_*" + binary + "*")
+    corsika_filenames = glob.glob(search_expr)
+    corsika_filenames.sort()
+    if len(corsika_filenames) > 0:
+        corsika_filename = corsika_filenames[0]
+        return
+    
+    raise RuntimeError(f"cannot find corsika binary {binary}")
 
