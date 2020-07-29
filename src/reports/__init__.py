efd47acb6183abcd20fc9c7589fd78bb9d54826b
Marcel Köpke <marcel.koepke@gmail.com>
2020-07-29 15:42:47 +0200

    new pathfinder for analysis

--- src/reports/__init__.py
+++ src/reports/__init__.py
@@ -4,6 +4,8 @@ import os
 import lazy_import
 if "lazy_import" in os.environ:
     lazy_import.lazy_module("src.reports.figures")
+    lazy_import.lazy_module("src.reports.analysis")
 
 from . import figures
+from . import analysis
 
