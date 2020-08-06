165264d785b402f958072a7a82a2a344292d1346
Marcel Köpke <marcel.koepke@gmail.com>
2020-08-06 15:08:39 +0200

    src/data/external documentation

--- src/data/external/__init__.py
+++ src/data/external/__init__.py
@@ -1,2 +1,4 @@
+"""Package to process external data files."""
+
 from .path import get_path
 
--- src/data/external/path.py
+++ src/data/external/path.py
@@ -3,5 +3,11 @@ import os
 from .. import get_path as get_parent_path
 
 def get_path():
+    """Return data external path, i.e. repobase/data/external.
+    
+    Returns
+    -------
+    datapath : str
+    """
     return os.path.join(get_parent_path(), "external")
 
